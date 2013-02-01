6cdab3a35be9e85aaa2a64179480fb3d496b7e78
Peter Goodhall <peter@magicbug.co.uk>
2013-02-01 17:25:22 +0000

    Added automation script

---
+++ Monitoring Scripts/start-spotsucker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TEST=`ps ax | grep '.spotsucker.' | grep -v 'grep'  ||  .spotsucker.`
+
+if [ "$TEST" = "" ]; then
+	echo "Error, Script not running..."
+	nohup /usr/home/bwirel/public_html/dxcluster/start-spotsucker.sh &
+else
+	echo $TEST
+	echo "Script is running..."
+fi
